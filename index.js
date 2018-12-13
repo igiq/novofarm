b8b592b5b5a37edd6f3b410502aaf1505604bd3c
火猪 <Igor_MAC@VERAs-bae.local>
2018-12-13 23:43:47 +0800

    still thinking about the layout of product page

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var app = express();
 
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname+'/public'));
 
-
+console.log(__dirname)
 
 app.get('/',function(req,res){
     res.render ('landing')
